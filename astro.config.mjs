e28c017d8fe77029e8ada71d63736f80390c5d83
iammarcmason <iammarcmason@googlemail.com>
2023-06-23 12:37:49 +0100

    new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
-
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  // ...
-  site: "https://thedatadecorator.com",
-  integrations: [sitemap()],
+  site: "https://astroship.web3templates.com",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()]
 });
